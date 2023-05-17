13d6f794f933e3eae104c0f1605ffb5bb0426bb1
Ryan McDonagh <laslomcd@gmail.com>
2023-05-17 08:48:52 -0400

    display records

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::factory(5)->create()->each(function($u){
+               $u->questions()
+                   ->saveMany(Question::factory(rand(1,5)))->create([
+                       'user_id' => User::find(rand(1,5))->id
+                   ]);
+        });
+
+//         User::factory()->count(3)
+//             ->has(Question::factory()->count(rand(1,10)))
+//             ->create();
+
     }
 }
